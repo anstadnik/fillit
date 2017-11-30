89717d85fb6109908ed8b0c01c5fee4a4d55ffd7
Andrii STADNIK <astadnik@student.unit.ua>
2017-11-30 20:33:41 +0200

    It passes the norminette

--- check.c
+++ check.c
@@ -6,7 +6,7 @@
 /*   By: lburlach <lburlach@student.unit.ua>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/19 17:31:30 by lburlach          #+#    #+#             */
-/*   Updated: 2017/11/30 19:55:20 by astadnik         ###   ########.fr       */
+/*   Updated: 2017/11/30 20:32:28 by astadnik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 ** This func checks whether the tetrimino is T_shaped.
 */
 
-int		check_t(char *buf, int count)
+int				check_t(char *buf, int count)
 {
 	if ((count < 11) && (buf[count + 5] == '#' && (buf[count + 6] == '#'
 				|| buf[count + 4] == '#') && buf[count + 10] == '#'))
@@ -33,14 +33,15 @@ int		check_t(char *buf, int count)
 		return (0);
 }
 
-static char	minsize(char first, char last)
+static char		minsize(char first, char last, char size)
 {
-	char	max;
+	char		max;
 
 	max = (last + 1) % 5 - getindex(first, 2);
 	max = max > 0 ? max : -max;
-	return (max > (last + 1) / 5 - getindex(first, 1) ? 
+	max = (max > (last + 1) / 5 - getindex(first, 1) ?
 		max + 1 : (last + 1) / 5 - getindex(first, 1) + 1);
+	return (max > size ? max : size);
 }
 
 /*
@@ -50,42 +51,31 @@ static char	minsize(char first, char last)
 
 static	int		check_n_count(char *buf, t_params *params, int *t_tet)
 {
-	int count;
-	int	t_count;
-	int r;
-	char	first;
+	int			count;
+	int			t_count;
+	int			r;
+	char		first;
 
-	count = 0;
-	t_count = 1;
+	count = -1;
+	t_count = 0;
 	r = 0;
-	while (buf[count])
-	{
-		if (!(buf[count] == '.' || buf[count] == '#' || buf[count] == '\n'))
-			return (0);
+	while (buf[++count])
 		if (buf[count] == '#')
 		{
 			if (!r++)
 				first = setindex((count + 1) / 5, (count + 1) % 5);
 			if (r == 4)
-				params->size = params->size > minsize(first, count) ? params->size :
-					minsize(first, count);
+				params->size = minsize(first, count, params->size);
 			if (check_t(buf, count))
 				(*t_tet)++;
-			if (buf[count + 1] == '#')
-				t_count++;
-			if (buf[count + 5] == '#')
-				t_count++;
-			if (buf[count - 1] == '#')
-				t_count++;
-			if (buf[count - 5] == '#')
-				t_count++;
-		}	
-		count++;
-		if ((!((count + 1) % 5) || count == 20) && buf[count] != '\n')
+			t_count += (buf[count + 1] == '#') + (buf[count + 5] == '#') +
+				(buf[count - 1] == '#') + (buf[count - 5] == '#');
+		}
+		else if (!(buf[count] == '.' || buf[count] == '\n') ||
+			((!((count + 1) % 5) || count == 20) && buf[count] != '\n'))
 			return (0);
-	}
 	params->amount++;
-	return ((t_count < 6) || r != 4) ? 0 : 1;
+	return ((t_count < 5) || r != 4) ? 0 : 1;
 }
 
 /*
@@ -95,7 +85,7 @@ static	int		check_n_count(char *buf, t_params *params, int *t_tet)
 
 static	void	count_size_of_sq(t_params *params, int t_tet)
 {
-	char	size;
+	char		size;
 
 	if (t_tet % 2)
 		size = ft_sqrt((params->amount) * 4 + 2);
@@ -122,10 +112,9 @@ t_params		*check(char *str)
 		return (NULL);
 	params->amount = 0;
 	params->size = 0;
+	buf[20] = '\n';
 	while ((ret = read(fd, &buf, 21)))
 	{
-		if (ret == 20 && params->amount == 0)
-			buf[20] = '\n';
 		if (ret < 20 || ret > 21 || !check_n_count(buf, params, &t_tet))
 			return (NULL);
 		if (ret == 20)
